4c000d1f1b6e93b5b61e45076b087377e1a954f0
gititinyoursoul <marius.hartmann@hotmail.com>
2024-10-28 15:13:23 +0100

    add sales_order_details model

---
+++ dbt_tutorial/models/warehouse/sales_order_detail.sql
@@ -0,0 +1,12 @@
+SELECT 
+    salesorderid as salesorder_id, 
+    salesorderdetailid as salesorderdetail_id, 
+    carriertrackingnumber, 
+    orderqty as order_qty, 
+    productid as product_id, 
+    specialofferid as specialoffer_id, 
+    unitprice, 
+    unitpricediscount, 
+    rowguid as row_id, 
+    modifieddate as modified_date
+from {{ source('sales', 'salesorderdetail') }}
