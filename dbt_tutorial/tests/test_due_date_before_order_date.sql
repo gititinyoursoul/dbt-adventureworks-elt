239007d9b30c6a86d1b731fa69cb15568de24c52
gititinyoursoul <marius.hartmann@hotmail.com>
2024-11-03 15:01:22 +0100

    fix refrence in test

--- dbt_tutorial/tests/test_due_date_before_order_date.sql
+++ dbt_tutorial/tests/test_due_date_before_order_date.sql
@@ -1,4 +1,4 @@
 SELECT due_date, order_date, count(1) as occurrences
-FROM {{ ref('sales_order_header') }}
+FROM {{ ref('sales_order_header_transformed') }}
 WHERE due_date < order_date
 GROUP BY due_date, order_date
